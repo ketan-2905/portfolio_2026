50b24d7809e50d8599c5ab7de4dab902b5fb59d3
ketan-2905 <ketangaikwad2905@gmail.com>
2026-04-24 21:04:49 +0530

    Analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import PortfolioPage from './components/Portfolio'
 import HackathonCalendar from './components/HackathonCalendar'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PortfolioPage />} />
-      <Route path="/cal" element={<HackathonCalendar />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<PortfolioPage />} />
+        <Route path="/cal" element={<HackathonCalendar />} />
+      </Routes>
+      <Analytics />
+    </>
   )
 }
 
